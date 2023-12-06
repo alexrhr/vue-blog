50bf1237923d3b98ab429490adef30c61da23837
timst <SpamHammerNGE@web.de>
2023-12-06 15:49:52 +0100

    hinzufügen des JSON Files

--- src/views/posts/Overview.vue
+++ src/views/posts/Overview.vue
@@ -1,3 +1,5 @@
+<!--
+
 <script setup>
 import PostCard from "@/components/PostCard.vue";
 </script>
@@ -12,6 +14,52 @@ import PostCard from "@/components/PostCard.vue";
           cols="auto"
       >
         <PostCard :title="'Post no ' + i"/>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+-->
+
+
+
+
+<script setup>
+import PostCard from "@/components/PostCard.vue"
+import {onMounted, ref} from "vue"
+import {collection, getDocs} from 'firebase/firestore'
+import db from '/src/views/db'
+
+const avatars = ref([
+'/female_avatar.jpeg',
+'/male_avatar.png',
+'/user1-128x128.jpg',
+])
+const posts = ref([])
+
+onMounted(async () => await loadPostings())
+
+// loads all postings from FireStore
+async function loadPostings() {
+const postings = await getDocs(collection(db, "posts"))
+postings.forEach(post => {
+const currentPost = {
+id: post.id,
+title: post.data().title,
+body: post.data().body,
+categories: post.data().categories?.map(cat => cat.id)
+}
+posts.value.push(currentPost)
+})
+}
+
+</script>
+
+<template>
+  <v-container>
+    <h1>Posts overview</h1>
+    <v-row id="overview">
+      <v-col v-for="post,i in posts" :key="i" cols="auto">
+        <PostCard :post="post" :avatar="avatars[i % 3]"/>
       </v-col>
     </v-row>
   </v-container>
