5efc2c2792c7b0a4f6a5f22d0f937acdea9120f7
timst <SpamHammerNGE@web.de>
2023-12-08 11:52:03 +0100

    hinzufügen des JSON Files

--- src/views/posts/Read.vue
+++ src/views/posts/Read.vue
@@ -6,6 +6,8 @@ import { useRouter } from 'vue-router';
 
 const router = useRouter();
 const users = ref([]);
+const displayedUser = ref(null); // Hier wird der aktuell angezeigte Benutzer gespeichert
+
 
 onMounted(async () => {
   await loadUsers();
@@ -21,28 +23,29 @@ async function loadUsers() {
       id: doc.id,
       name: userData.name,
       balance: userData.balance,
+      verhaelt: userData.verhaelt,
       geburtstag: userData.geburtstag,
+      aktiendepot: userData.aktiendepot,
+      debt: userData.debt,
+      gehalt: userData.Gehalt,
+      sonstigeein: userData.sonstigeein,
+      ratenzahlung: userData.ratenzahlung,
+      sonstigeaus: userData.sonstigeaus,
     };
   });
 }
 
-/*function viewUserDetails(userId) {
-  router.push(`/users/${userId}`);
-}*/
 
-function viewUserDetails(userId) {
-  router.push({ path: `/users/${userId}` });
+
+function viewUserDetails(user) {
+  displayedUser.value = { ...user }; // Hier wird der Benutzer für die Anzeige kopiert
 }
 
-function viewUserDetails2(userId) {
-  // Debugging: Logge die Benutzer-ID und den generierten Pfad
-  console.log('Benutzer-ID:', userId);
-  const path = `/users/${userId}`;
-  console.log('Generierter Pfad:', path);
-
-  // Versuche, zur Route zu navigieren
-  router.push({ path });
+function closeDialog() {
+  displayedUser.value = null; // Schließe das Dialogfenster
 }
+
+
 
 
 async function deleteUser(userId) {
@@ -65,12 +68,54 @@ async function deleteUser(userId) {
             <v-btn color="red-darken-4" variant="elevated" @click="deleteUser(user.id)">
               Löschen
             </v-btn>
-            <v-btn color="primary" variant="elevated" @click="viewUserDetails(user.id)">
+            <v-btn color="primary" variant="elevated" @click="viewUserDetails(user)">
               Ansehen
             </v-btn>
           </v-card-actions>
         </v-card>
       </v-col>
     </v-row>
+
+    <!-- Dialogfenster für die Anzeige der Benutzerdaten -->
+    <v-dialog v-if="displayedUser" v-model="displayedUser" persistent>
+      <v-card>
+        <v-card-title>Anzeige der Daten von {{ displayedUser.name }}</v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-col cols="12">
+              <strong>Name:</strong> {{ displayedUser.name }}
+            </v-col>
+            <v-col cols="12">
+              <strong>Kontostand:</strong> {{ displayedUser.balance }}
+            </v-col>
+            <v-col cols="12">
+              <strong>Verhältnis:</strong> {{ displayedUser.verhaelt }}
+            </v-col>
+            <v-col cols="12">
+              <strong>Geburtstag:</strong> {{ displayedUser.geburtstag }}
+            </v-col>
+            <v-col cols="12">
+              <strong>Aktiendepot:</strong> {{ displayedUser.aktiendepot }}
+            </v-col>
+            <v-col cols="12">
+              <strong>Schulden:</strong> {{ displayedUser.debt }}
+            </v-col>
+            <v-col cols="12">
+              <strong>Gehalt:</strong> {{ displayedUser.gehalt }}
+            </v-col>
+            <v-col cols="12">
+              <strong>Sonstige Einzahlung:</strong> {{ displayedUser.sonstigeein }}
+            </v-col>
+            <v-col cols="12">
+              <strong>Ratenzahlung:</strong> {{ displayedUser.ratenzahlung }}
+            </v-col>
+            <v-col cols="12">
+              <strong>Sonstige Ausgaben:</strong> {{ displayedUser.sonstigeaus }}
+            </v-col>
+          </v-container>
+          <v-btn @click="closeDialog" color="primary">Schließen</v-btn>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
