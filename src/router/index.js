07a7e97b6d3deda5cb7a3c3a9f02bca0da111be6
artingo <alfred.walther@artingo.net>
2023-12-21 10:03:08 +0100

    Removed unused routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Overview from "@/views/posts/Overview.vue";
 import Users from "@/views/Users.vue";
-import Categories from "@/views/Categories.vue";
 import Read from "@/views/posts/Read.vue";
 import Read2 from "@/views/posts/Read2.vue";
 import Create from "@/views/posts/Create.vue";
@@ -8,13 +7,8 @@ import Create from "@/views/posts/Create.vue";
 const routes = [
   {path: '/startseite', component: Overview},
   {path: '/', component: Overview},
-  //{path: '/posts/:id', component: Read},
   {path: '/users/:id', component: Read2},
   {path: '/hinzufügen', component: Create},
-  {path: '/posts/update', component: Overview},
-  {path: '/posts/save', component: Overview},
-  {path: '/posts/delete', component: Overview},
-  {path: '/categories', component: Categories},
   {path: '/finanzen', component: Read},
   {path: '/posts/create', component: Users},
 ];
